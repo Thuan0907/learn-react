108c8007214753f8a8a4b109197f3e5637c9a754
Thuan0907 <jinkin0019@gmail.com>
2022-12-23 18:15:42 +0700

    video16

---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        {" "}
+        My first component
+        {Math.random()}
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
