072c10971b26e9602e35e9ffb25c95e466716d06
Thuan0907 <jinkin0019@gmail.com>
2022-12-31 18:01:57 +0700

    video47

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import logo from "./logo.svg";
 import "./App.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./components/MyComponent";
+// import MyComponent from "./components/MyComponent";
 import React from "react";
+import Header from "./components/Header/Header";
 
 const App = () => {
-  return <div className="app-container">Hello world</div>;
+  return (
+    <div className="app-container">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
