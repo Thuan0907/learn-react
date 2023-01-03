4f3796069526ff70a588854981d1b0d7475f1062
Thuan0907 <jinkin0019@gmail.com>
2023-01-03 16:57:22 +0700

    video48

--- src/App.js
+++ src/App.js
@@ -5,11 +5,24 @@ import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
 // import MyComponent from "./components/MyComponent";
 import React from "react";
 import Header from "./components/Header/Header";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app-container">
       <Header />
+      <div>
+        test Link
+        <div>
+          <button>
+            {" "}
+            <Link to="/users">Go to User Page </Link>{" "}
+          </button>
+          <button>
+            <Link to="/admin">Go to Admin Page </Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
