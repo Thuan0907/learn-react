9fc7b963cb6a7b5e02146d6780de3e3806cdb445
Thuan0907 <jinkin0019@gmail.com>
2023-01-03 17:35:59 +0700

    video49

--- src/App.js
+++ src/App.js
@@ -5,22 +5,18 @@ import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
 // import MyComponent from "./components/MyComponent";
 import React from "react";
 import Header from "./components/Header/Header";
-import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app-container">
-      <Header />
-      <div>
-        test Link
-        <div>
-          <button>
-            {" "}
-            <Link to="/users">Go to User Page </Link>{" "}
-          </button>
-          <button>
-            <Link to="/admin">Go to Admin Page </Link>
-          </button>
+      <div className="header-container">
+        <Header />
+      </div>
+      <div className="main-container">
+        <div className="sidenav-container"></div>
+        <div className="app-content">
+          <Outlet></Outlet>
         </div>
       </div>
     </div>
