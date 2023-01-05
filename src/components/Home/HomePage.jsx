b40ebdf624fa357a7363becbdcfce7c6433560d7
Thuan0907 <jinkin0019@gmail.com>
2023-01-05 11:59:50 +0700

    video52

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import videoHomepage from "../../assets/video-homepage.mp4";
 
 export default function HomePage(props) {
-  return <div className="homepage-container">HomePage</div>;
+  return (
+    <div className="homepage-container">
+      <video autoPlay muted loop>
+        <source src={videoHomepage} />
+      </video>
+    </div>
+  );
 }
