ee52bdb359585f97a8ec893f7f2140181ed7fa55
Thuan0907 <jinkin0019@gmail.com>
2023-03-02 14:34:27 +0700

    video78

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import videoHomepage from "../../assets/video-homepage.mp4";
+import { useSelector } from "react-redux";
 
 export default function HomePage(props) {
+  const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+  const account = useSelector((state) => state.user.account);
+
+  console.log(
+    ">>> check isAuthenticated: ",
+    isAuthenticated,
+    ">>> check account: ",
+    account
+  );
   return (
     <div className="homepage-container">
       <video autoPlay muted loop>
